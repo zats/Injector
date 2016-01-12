8c3bcff0e9bae353146abbdaeb53c918f538e38d
Sash Zats <sash@zats.io>
2016-01-12 09:55:01 -0800

    Remove Foundation dependancy

--- Sources/Injector.h
+++ Sources/Injector.h
@@ -6,13 +6,12 @@
 //
 //
 
-#import <Foundation/Foundation.h>
 
 //! Project version number for Injector.
-FOUNDATION_EXPORT double InjectorVersionNumber;
+extern double InjectorVersionNumber;
 
 //! Project version string for Injector.
-FOUNDATION_EXPORT const unsigned char InjectorVersionString[];
+extern const unsigned char InjectorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Injector/PublicHeader.h>
 
